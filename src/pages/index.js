6e9b09555a898ad903bfed591c89d5d33f9ecd41
Victor Regueira <victor1mrs@hotmail.com>
2023-03-28 12:49:41 -0300

    navbar added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
     <section>
+      <Navbar/>
       <div>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
