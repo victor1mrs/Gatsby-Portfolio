12650fa6f0b258955c04214ec0bed4dbaa4c4480
Victor Regueira <victor1mrs@hotmail.com>
2023-03-28 15:39:21 -0300

    using static files and graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Layout from '../components/Layout';
 import { Link } from 'gatsby';
 import * as styles from '../styles/home.module.css'
 
-const Home = () => {
+const Home = ({data}) => {
+  // const { title, description } = data.site.siteMetadata;
   return (
     <Layout>
       <section className={styles.header} >
@@ -13,9 +14,25 @@ const Home = () => {
           <p>Ux designer & web developer based in UK</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
+        <img src="/banner.png" alt="site banner" style={{maxWidth: '100%'}}></img>
+        {/* <p>{title} - {description}</p> */}
       </section>
     </Layout>
   )
 }
 
 export default Home;
+
+//  --  This is a Page Query  --
+// This is the way to run grapql querys on a page component, in here we can use query variables
+
+// export const query = graphql`
+//   query MyQuery {
+//     site {
+//       siteMetadata {
+//         description
+//         title
+//       }
+//     }
+//   }
+// `
