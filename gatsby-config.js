df1279e997e9d933ea2b449f3264920b37937928
Victor Regueira <victor1mrs@hotmail.com>
2023-03-29 14:26:42 -0300

    adding optimized loaded images

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,9 @@
 module.exports = {
   plugins: [
     'gatsby-transformer-remark',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp', // Needed for dynamic images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -26,6 +29,15 @@ module.exports = {
         name: `projects`,
         // Path to the directory
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `images`,
+        // Path to the directory
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
